5bd6d7adccf41a6d72f5f116b5c78c78af2fd1ab
Koutyboy <victor.karlsson.github@gmail.com>
2015-11-19 14:22:50 +0100

    Implemented the framework for the HUB Mode/State.

---
+++ Assets/Scripts/Game States/HUBState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents the state in which the player chooses the task for the day.
+/// </summary>
+public class HUBState : State {
+
+    public override void Update(float dt)
+    {
+        // base.Update(dt); Don't tick time when in HUB State.
+    }
+
+    public override string ToString()
+    {
+        return "HUB State";
+    }
+}
