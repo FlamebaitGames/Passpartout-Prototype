e103db8535a9d175b53aab1a2f54434cbd9e5eb1
Koutyboy <victor.karlsson.github@gmail.com>
2015-11-19 14:21:03 +0100

    Created the framework for the finite state machine

---
+++ Assets/Scripts/Game States/FSM.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FSM {
+    Stack<State> states = new Stack<State>();
+
+	// Update is called once per frame
+	public void Update (float dt) {
+        State current = Peek();
+        if (current != null)
+        {
+            current.Update(dt);
+        }
+	}
+
+    public State Peek()
+    {
+        return (states.Count != 0) ? states.Peek() : null;
+    }
+
+    public void Push(State state)
+    {
+        State old = Peek();
+        if (old != null)
+        {
+            old.Exit();
+        }
+        states.Push(state);
+        state.Enter();
+    }
+
+    public State Pop()
+    {
+        return (states.Count != 0) ? states.Pop() : null;
+    }
+}
