79baf364b5cb58d27ed6fc0f8b7b1d211252f682
Koutyboy <victor.karlsson.github@gmail.com>
2015-11-19 14:22:15 +0100

    Implemented the framework for Shop Mode/State.

---
+++ Assets/Scripts/Game States/ShopState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents the state in which the user opens the shop and sells paintings, allowing customers in etc.
+/// </summary>
+public class ShopState : State {
+
+    public override void Update(float dt)
+    {
+        base.Update(dt);
+    }
+
+    public override string ToString()
+    {
+        return "Shop State";
+    }
+}
