b7191a2ec655497136947f224881775b886a8026
Koutyboy <victor.karlsson.github@gmail.com>
2015-11-19 14:22:37 +0100

    Implemented the framework for the Paint Mode/State.

---
+++ Assets/Scripts/Game States/PaintState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents the state in which the user makes paintings (including, naming, setting base price etc)
+/// </summary>
+public class PaintState : State {
+
+    public override void Update(float dt)
+    {
+        base.Update(dt);
+    }
+
+    public override string ToString()
+    {
+        return "Paint State";
+    }
+}
