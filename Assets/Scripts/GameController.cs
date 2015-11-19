3fbd1dc9b0819e10a5295f9ea218b44f7a809989
Koutyboy <victor.karlsson.github@gmail.com>
2015-11-19 14:26:23 +0100

    Merged GameController and Game (since they are essentially the same thing) and removed Game.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
+    FSM finiteStateMachine = new FSM();
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        finiteStateMachine.Push(new HUBState());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        finiteStateMachine.Update(Time.deltaTime);
+    }
 }
