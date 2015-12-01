547efe33af3a6b8732398cc57272b4fbd3246321
Ryxali <ryxali@gmail.com>
2015-12-01 13:36:16 +0100

    added lose state

---
+++ Assets/Scripts/Game States/LoseState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseState : State {
+
+    public override void Enter()
+    {
+        menuPanels.SetPanelsToShow(MenuPanels.Panel.LOSE_PANEL | MenuPanels.Panel.MONEY_PANEL);
+        menuPanels.context.weeklyExpensesText.text = "You have lost";
+    }
+
+    public override void Update(float dt)
+    {
+
+    }
+
+    
+}
