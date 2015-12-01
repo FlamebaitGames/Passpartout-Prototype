2aa9a495002eec574a69d8e5100a2104316db898
Ryxali <ryxali@gmail.com>
2015-12-01 16:38:31 +0100

    best paint

--- Assets/PaintComponent.cs
+++ Assets/PaintComponent.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class PaintComponent : MonoBehaviour {
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+public class PaintComponent : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler {
     public Texture2D canvas;
     public Camera cam;
+
+
+    private Vector2 lastMousePoistion = Vector2.zero;
 	// Use this for initialization
 	void Start () {
         var original = gameObject.GetComponent<UnityEngine.UI.RawImage>().mainTexture as Texture2D;
 
-        canvas = new Texture2D(original.width, original.height);
+        canvas = new Texture2D((int)GetComponent<RectTransform>().rect.width, (int)GetComponent<RectTransform>().rect.height);
         gameObject.GetComponent<UnityEngine.UI.RawImage>().texture = canvas;
 
         for (int y = 0; y < canvas.height; ++y)
@@ -21,53 +25,55 @@ public class PaintComponent : MonoBehaviour {
         canvas.Apply();
     }
 
+    public void OnPointerDown(PointerEventData data)
+    {
+    }
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+    public void OnDrag(PointerEventData data)
+    {
+        RectTransform rect = GetComponent<RectTransform>();
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, data.pointerCurrentRaycast.screenPosition, data.pressEventCamera, out pos);
+
+        pos.y = rect.rect.height - Mathf.Abs(pos.y);
+        pos.y /= rect.rect.height / canvas.height;
+        pos.x /= rect.rect.width / canvas.width;
+        DrawCircle(canvas, (int)pos.x, (int)pos.y, 16, Color.black);
+        canvas.Apply();
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+
 	// Update is called once per frame
 	void Update () {
         /*
-        if (!Input.GetMouseButton(0))
-            return;
-
-        RaycastHit hit;
-        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
-            return;
-
-        Renderer rend = hit.transform.GetComponent<Renderer>();
-        MeshCollider meshCollider = hit.collider as MeshCollider;
-        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
-            return;
-
-        Texture2D tex = canvas;
-        Vector2 pixelUV = hit.textureCoord;
-        pixelUV.x *= tex.width;
-        pixelUV.y *= tex.height;
-        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
-        tex.Apply();
-        */
-
-        Vector2 s = new Vector2(canvas.width, canvas.height);
-        Vector2 p = gameObject.GetComponent<RectTransform>().position;
-        Rect r = gameObject.GetComponent<RectTransform>().rect;
-        Vector2 m = Input.mousePosition;
-        float relWidth = Screen.width / 1920.0f;
-        float relHeight = Screen.height / 1080.0f;
-
-        m.y *= relHeight;
-        m.x *= relWidth;
-        // Bounds check
-        if (r.Contains(m))
+        if (Input.GetMouseButton(0))
         {
-            int x = (int)(m.x);
-            int y = (int)(m.y);
+            RectTransform rect = GetComponent<RectTransform>();
+            Vector2 pos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, Input.mousePosition, null, out pos);
 
-            DrawCircle(canvas, (int)m.x, (int)m.y,16, Color.black);
+            pos.y = rect.rect.height - Mathf.Abs(pos.y);
+            pos.y /= rect.rect.height / canvas.height;
+            pos.x /= rect.rect.width / canvas.width;
+            DrawCircle(canvas, (int)pos.x, (int)pos.y, 16, Color.black);
             canvas.Apply();
         }
+        */
 	}
 
 
     public void DrawCircle(Texture2D tex, int cx, int cy, int r, Color col)
     {
-        var pixels = canvas.GetPixels32();
+        Color32[] pixels = canvas.GetPixels32();
+
         int x, y, px, nx, py, ny, d;
 
         for (x = 0; x <= r; x++)
@@ -80,13 +86,21 @@ public class PaintComponent : MonoBehaviour {
                 py = cy + y;
                 ny = cy - y;
 
+
+                pixels[py * canvas.width + px] = col;
+                pixels[py * canvas.width + nx] = col;
+                pixels[ny * canvas.width + px] = col;
+                pixels[ny * canvas.width + nx] = col;
+
+                /*
                 tex.SetPixel(px, py, col);
                 tex.SetPixel(nx, py, col);
 
                 tex.SetPixel(px, ny, col);
                 tex.SetPixel(nx, ny, col);
-
+                */
             }
         }
+        tex.SetPixels32(pixels);
     }
 }
