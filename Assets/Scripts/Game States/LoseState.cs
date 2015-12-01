fd14fd0f23755a75769dcf5aae2d92733b0202b0
Ryxali <ryxali@gmail.com>
2015-12-01 13:59:49 +0100

    lose state no longer influences the text of another panel

--- Assets/Scripts/Game States/LoseState.cs
+++ Assets/Scripts/Game States/LoseState.cs
@@ -6,7 +6,7 @@ public class LoseState : State {
     public override void Enter()
     {
         menuPanels.SetPanelsToShow(MenuPanels.Panel.LOSE_PANEL | MenuPanels.Panel.MONEY_PANEL);
-        menuPanels.context.weeklyExpensesText.text = "You have lost";
+        //menuPanels.context.weeklyExpensesText.text = "You have lost";
     }
 
     public override void Update(float dt)
