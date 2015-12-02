66ccbe7bbaca8ae432df6815c80729538e5bdd64
ViktorZryd <viktorzryd@gmail.com>
2015-12-02 11:10:44 +0100

    Initial audio commit
    Added audio files and music manager + ambience

---
+++ Assets/Scripts/MusicManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManagerScript : MonoBehaviour
+{
+	public AudioSource musicLoop;
+	public AudioSource musicInit;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		musicInit.Play();
+		musicLoop.PlayDelayed(musicInit.clip.length);
+	}
+	
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+}
+
